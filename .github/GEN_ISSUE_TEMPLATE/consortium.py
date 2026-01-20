0c05f8a6069469378c85750c74009ae0bc38d2b3
Daniel Ellis <daniel.ellis@ext.esa.int>
2026-01-20 13:20:17 +0000

    Consortium templates

--- .github/GEN_ISSUE_TEMPLATE/consortium.py
+++ .github/GEN_ISSUE_TEMPLATE/consortium.py
@@ -1,5 +1,8 @@
 # Consortium Template Configuration
 
+import cmipld
+from cmipld.utils.ldparse import name_extract
+
 TEMPLATE_CONFIG = {
     'name': 'Add/Modify: Consortium',
     'description': 'Add or modify a consortium (group of institutions) in WCRP Universe',
@@ -11,7 +14,11 @@ TEMPLATE_CONFIG = {
 # Data for this template
 # The 'institutions' key uses a special prefix to fetch from the organisation collection
 # filtering by type 'wcrp:institution'
+
+org = name_extract(cmipld.get('universal:organisation/graph.jsonld', depth=0))
+inst = [f"{k} : {v.get('ui_label','')}" for k,v in org.items() if 'ror' in v]
+
 DATA = {
     'issue_kind': ['New', 'Modify'],
-    'institutions': 'wcrp:institution'  # This tells the generator to fetch all institutions
+    'institutions': inst  # This tells the generator to fetch all institutions
 }
